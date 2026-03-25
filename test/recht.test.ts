2b82d8f5e5ef24e0051d582d14856a2b27b73d08
Elvaleryn <eraykaya121@gmail.com>
2026-03-25 13:23:00 +0300

    Typescript migration

---
+++ test/recht.test.ts
@@ -0,0 +1,151 @@
+import { describe, it, expect } from 'vitest'
+import Recht from '../src'
+import * as fixture from './helpers/fixture'
+
+describe('Recht', () => {
+  it('Allows a rule', () => {
+    const recht = new Recht()
+    const rules = [['ALLOW', 'x'] as const]
+    recht.rules = rules
+
+    expect(recht.check('x')).toBe(true)
+    expect(Recht.check({ rules }, 'x')).toBe(true)
+  })
+
+  it('Supports constructing with definitions', () => {
+    const recht = new Recht({
+      dimensions: [['x', 'y']] as const,
+      rules: [['ALLOW', 'x']] as const
+    })
+
+    expect(recht.check('x')).toBe(true)
+    expect(recht.check('y')).toBe(false)
+  })
+
+  it('Allows wildcard rules', () => {
+    const recht = new Recht()
+    const rules = [['ALLOW', '*'] as const]
+    recht.rules = rules
+
+    expect(recht.check('x')).toBe(true)
+    expect(Recht.check({ rules }, 'x')).toBe(true)
+  })
+
+  it('Denies wildcard rules', () => {
+    const recht = new Recht()
+    const rules = [['DENY', '*'] as const]
+    recht.rules = rules
+
+    expect(recht.check('x')).toBe(false)
+    expect(Recht.check({ rules }, 'x')).toBe(false)
+  })
+
+  it('With dimensions, denies a condition out of dimensions even though wildcard is used', () => {
+    const recht = new Recht()
+    const rules = [['ALLOW', '*'] as const]
+    const dimensions = [['x', 'y']] as const
+    recht.rules = rules
+    recht.dimensions = dimensions
+
+    expect(recht.check('z')).toBe(false)
+    expect(Recht.check({ rules, dimensions }, 'z')).toBe(false)
+  })
+
+  it('Denies a rule', () => {
+    const recht = new Recht()
+    const rules = [['DENY', 'x'] as const]
+    recht.rules = rules
+
+    expect(recht.check('x')).toBe(false)
+    expect(Recht.check({ rules }, 'x')).toBe(false)
+  })
+
+  it('Rule order defines priority', () => {
+    const recht = new Recht()
+    recht.rules = [['DENY', 'x'] as const, ['ALLOW', 'x'] as const]
+
+    expect(recht.check('x')).toBe(false)
+
+    const rules = [['ALLOW', 'x'] as const, ['DENY', 'x'] as const]
+    recht.rules = rules
+
+    expect(recht.check('x')).toBe(true)
+    expect(Recht.check({ rules }, 'x')).toBe(true)
+  })
+
+  it('Denies a condition outside of ruleset', () => {
+    const recht = new Recht()
+    const rules = [['ALLOW', 'x'] as const]
+    recht.rules = rules
+
+    expect(recht.check('y')).toBe(false)
+    expect(Recht.check({ rules }, 'y')).toBe(false)
+  })
+
+  it('Performs a rule set', () => {
+    const recht = new Recht()
+    const { dimensions, rules, checkAssertions, closestAssertions } = fixture
+    recht.dimensions = dimensions
+    recht.rules = [...rules]
+
+    for (const { input, expected } of checkAssertions) {
+      expect(recht.check(...input)).toBe(expected.value)
+      expect(Recht.check({ rules, dimensions }, ...input)).toBe(expected.value)
+    }
+
+    for (const { input, expected } of closestAssertions) {
+      expect(recht.closest(...input)).toEqual(expected.conditions)
+      expect(Recht.closest({ rules, dimensions }, ...input)).toEqual(expected.conditions)
+
+      expect(recht.closestValue(...input)).toBe(expected.value)
+      expect(Recht.closestValue({ rules, dimensions }, ...input)).toBe(expected.value)
+
+      expect(recht.closestVerbose(...input)).toEqual(expected)
+      expect(Recht.closestVerbose({ rules, dimensions }, ...input)).toEqual(expected)
+    }
+  })
+
+  it('Check throws if no rules are set', () => {
+    const recht = new Recht()
+    expect(() => recht.check('test')).toThrowError('Please provide a rules array in order to use the check method.')
+    expect(() => Recht.check(undefined, 'test')).toThrowError('Please provide a rules array in order to use the check method.')
+    // @ts-expect-error testing null definitions
+    expect(() => Recht.check(null, 'test')).toThrowError('Please provide a rules array in order to use the check method.')
+  })
+
+  it('Check throws if no conditions are given', () => {
+    const recht = new Recht()
+    const rules = [['ALLOW', '*'] as const]
+    recht.rules = rules
+
+    expect(() => recht.check()).toThrowError('Please provide conditions as arguments to the check call.')
+    expect(() => Recht.check({ rules })).toThrowError('Please provide conditions as arguments to the check call.')
+  })
+
+  it('Check throws if a rule has an unknown direction', () => {
+    const recht = new Recht()
+    const rules = [['LET', '*'] as const]
+    // @ts-expect-error testing invalid action type 'LET'
+    recht.rules = rules
+
+    expect(() => recht.check('test')).toThrowError('Unknown action LET in rule "*" at index 0.')
+    // @ts-expect-error testing invalid action type 'LET'
+    expect(() => Recht.check({ rules }, 'test')).toThrowError('Unknown action LET in rule "*" at index 0.')
+  })
+
+  it('Closest throws if no dimensions are set', () => {
+    const recht = new Recht()
+    expect(() => recht.closest('test')).toThrowError('Please provide a dimensions array in order to use the closest method.')
+    expect(() => Recht.closest(undefined, 'test')).toThrowError('Please provide a dimensions array in order to use the closest method.')
+  })
+
+  it('Closest functions throw if no dimensions are set', () => {
+    const fns = [Recht.closest, Recht.closestValue, Recht.closestVerbose] as const
+
+    for (const fn of fns) {
+      // @ts-expect-error testing missing definitions and conditions
+      expect(() => fn()).toThrowError('Please provide a dimensions array in order to use the closest method.')
+    }
+  })
+})
+
---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  outDir: 'dist',
+  target: 'es2020',
+  platform: 'node',
+  cjsInterop: true
+})
+
